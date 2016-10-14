2575bea67cc2c2e98037d8b15f334f1322c0c8d6
k.kuzmin <k.kuzmin@npo-echelon.ru>
2016-10-14 18:05:13 +0300

    easy

--- pprint_json.py
+++ pprint_json.py
@@ -1,13 +1,19 @@
 import json
+import os
+import pprint
 
 
 def load_data(filepath):
-    pass
+    if not os.path.exists(filepath):
+        return None
+    with open(filepath, 'r') as json_file:
+        return json.load(json_file)
 
 
 def pretty_print_json(data):
-    pass
+    pprint.PrettyPrinter().pprint(data)
 
 
 if __name__ == '__main__':
-    pass
+    bars = load_data('bars.json')
+    pretty_print_json(bars)
